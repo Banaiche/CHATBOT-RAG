06eba15e20aee5488726f7004e6c18fb846a5066
banaiche <banaiche.abdelali@gmail.com>
2024-07-14 23:32:13 +0100

    Initial commit

---
+++ src/main/java/net/banaiche/chatbotrag/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package net.banaiche.chatbotrag.services;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/test/java/net/banaiche/chatbotrag/ChatbotRagApplicationTests.java
@@ -0,0 +1,13 @@
+package net.banaiche.chatbotrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
